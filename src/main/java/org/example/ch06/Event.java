48496b10b02b7ef441621470453661b2c7198654
Flre-fly <firefly_0@naver.com>
2022-12-14 16:41:35 +0900

    book: ch06 쿼리와 명령을 분리하라 - 쿼리와 명령을 분리했다. isSatified를 보는것만으로 이것이 쿼리구나라는것을알 수 있다. 명령을 호출할때는 부수효과에 주의할 수 있다 - 부수효과의 예시: 대입문 값대신 이 메서드의 return값을 써도된다.

--- src/main/java/org/example/ch06/Event.java
+++ src/main/java/org/example/ch06/Event.java
@@ -17,7 +17,6 @@ public class Event {
         if(from.getDayOfWeek() != schedule.getDayOfWeek() ||
                 !from.toLocalTime().equals(schedule.getFrom()) ||
                 !duration.equals(schedule.getDuration())) {
-            reschedule(schedule); // reschedule 메서드는 Event 객체의 상태를 수정한다!
             return false;
         }
 
